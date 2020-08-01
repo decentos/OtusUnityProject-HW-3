05815a0010967c439158374711251a63e684d94d
Vlad Matveev <matveev_vlad@me.com>
2020-08-01 10:25:10 +0300

    Add sounds for all action

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public float current = 3.0f;
+    public SoundPlay SoundPlay;
 
     public void ApplyDamage(float damage)
     {
         current -= damage;
+        SoundPlay.PlayDamage();
     }
 }
--- Assets/Scripts/SoundPlay.cs
+++ Assets/Scripts/SoundPlay.cs
@@ -4,6 +4,8 @@
 public sealed class SoundPlay : MonoBehaviour
 {
     [SerializeField] private AudioClip _clip;
+    [SerializeField] private AudioClip _damageClip;
+    [SerializeField] private AudioClip _deathClip;
     private AudioSource _audioSource;
 
     private void Start()
@@ -14,6 +16,18 @@ public sealed class SoundPlay : MonoBehaviour
     public void Play()
     {
         _audioSource.clip = _clip;
+        _audioSource.Play();
+    }
+
+    public void PlayDeath()
+    {
+        _audioSource.clip = _deathClip;
+        _audioSource.Play();
+    }
+
+    public void PlayDamage()
+    {
+        _audioSource.clip = _damageClip;
         _audioSource.Play();
     }
 }
